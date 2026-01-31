5d42d8d4e20c266fbeb07f9b61617f3872c0a924
olibreadstick <122502760+olibreadstick@users.noreply.github.com>
2026-01-31 14:17:40 -0500

    Tag feature

---
+++ Assets/Scripts/Player/TagPlayer1.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class SpriteChangeOnCOllision : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+
+    public Sprite originalSprite;
+    public Sprite alternateSprite;
+
+    private bool isAlternateSprite = false;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer != null)
+        {
+            originalSprite = spriteRenderer.sprite;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        ToggleSprite();
+    }
+
+    private void ToggleSprite()
+    {
+        if (spriteRenderer != null)
+        {
+            if (isAlternateSprite)
+            {
+                spriteRenderer.sprite = originalSprite;
+                isAlternateSprite = false;
+            }
+            else
+            {
+                spriteRenderer.sprite = alternateSprite;
+                isAlternateSprite = true;
+            }
+        }
+    }
+}
