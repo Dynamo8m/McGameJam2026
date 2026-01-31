e997ee87fdbb33a04651dba0e5ec1b90d1d8df74
olibreadstick <122502760+olibreadstick@users.noreply.github.com>
2026-01-31 11:44:25 -0500

    Two player controls

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed = 5;
     private Rigidbody2D body;
-    [SerializeField] private float speed;
+
+    private Vector2 movementInput;
+    //[SerializeField] private float speed;
 
     private void Awake()
     {
@@ -14,29 +21,34 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         //Player movement
-        float horizontalInput = Input.GetAxis("Horizontal");
-        body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
+        //float horizontalInput = Input.GetAxis("Horizontal");
+       // body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
+
+        transform.Translate(new Vector3(movementInput.x, 0, movementInput.y) * speed * Time.deltaTime);
 
 
         // Flipping Character when moving
-        if (horizontalInput > 0.01f)
-        {
-            transform.localScale = Vector3.one;
-        }
+        //if (movementInput.x > 0.01f)
+        //{
+        //    transform.localScale = Vector3.one;
+        //}
 
-        else if (horizontalInput > -0.01f)
-        {
-            transform.localScale = new Vector3(-1,1,1);
-        }
+        //else if (movementInput.x < -0.01f)
+        //{
+        //    transform.localScale = new Vector3(-1,1,1);
+        //}
 
 
         // Jumping
-        if (Input.GetKey(KeyCode.Space))
-        {
-            body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
-        }
+        //if (Input.GetKey(KeyCode.Space))
+        //{
+        //    body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
+        //}
 
-
+        
+        
 
     }
+    public void OnMove(InputAction.CallbackContext context) => movementInput = context.ReadValue<UnityEngine.Vector2>();
+
 }
