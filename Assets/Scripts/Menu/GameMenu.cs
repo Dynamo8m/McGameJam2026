2c59f135888fd5dad7008eb9eb3b1dad9d685988
Ian Ma <ian.is.life123@gmail.com>
2026-01-31 19:25:23 -0500

    Organized Files, Created WinScene

--- Assets/Scripts/Menu/GameMenu.cs
+++ Assets/Scripts/Menu/GameMenu.cs
@@ -10,10 +10,6 @@ public class GameMenu : MonoBehaviour
         SceneManager.LoadScene(gameSceneName);
     }
 
-    public void Options() 
-    {
-
-    }
     public void Quit() 
     {
         Application.Quit();
